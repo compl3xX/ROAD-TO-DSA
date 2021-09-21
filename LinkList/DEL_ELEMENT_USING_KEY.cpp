a896fd982dc6286287ba18a3296ecc990f83e5d0
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2021-09-21 19:10:10 +0530

    Create DEL_ELEMENT_USING_KEY.cpp

---
+++ LinkList/DEL_ELEMENT_USING_KEY.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+
+void printlist(node *n)
+{
+    while (n != NULL)
+    {
+        //cout << n;
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+void deletekey(node **headref, int key)
+{
+    node *temp = *headref;
+    node *prev = NULL;
+    if (temp != NULL && temp->data == key)
+    {
+        *headref = temp->next;
+        delete temp;
+        return;
+    }
+    else
+    {
+        while (temp != NULL && temp->data != key)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        if (temp == NULL)
+        {
+            return;
+        }
+        prev->next = temp->next;
+        delete temp;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    node *second = NULL;
+    node *third = NULL;
+
+    head = new node();
+    second = new node();
+    third = new node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    printlist(head);
+    deletekey(&head, 2);
+    cout << "\n";
+    printlist(head);
+
+    return 0;
+}
