7a66cbe1c364d71ee7f7793c37fe74e022998e44
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2021-10-23 18:28:57 +0530

    Add files via upload

---
+++ Stack/Stack_5_BALANCED_BRACKET.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool areBracketsBalanced(string str)
+{
+    stack<char>s;
+    char x;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if(str[i]=='('||str[i]=='{'||str[i]=='[')
+        {
+           s.push(str[i]);
+           continue;
+        }
+        if(s.empty()) return false;
+
+        switch(str[i])
+        {
+            case ')':
+
+             x=s.top();
+             s.pop();
+             if(x=='{'||x=='[')
+               return false ;
+               break;
+             
+             case '}':
+             x=s.top();
+             s.pop();
+             if(x=='('||x=='[')
+             return false;
+             break;
+              case ']':
+             x=s.top();
+             s.pop();
+             if(x=='('||x=='{')
+             return false;
+             break;
+
+        }
+    }
+    return  (s.empty());
+    
+}
+int main()
+
+{
+     string expr = "{()}[]";
+ 
+    // Function call
+    if (areBracketsBalanced(expr))
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+    return 0;
+    return 0;
+}
