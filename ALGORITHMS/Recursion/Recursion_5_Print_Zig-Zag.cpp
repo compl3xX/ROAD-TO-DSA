cf613572389a5e0f6415281986bc89c791e11245
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2021-11-15 22:56:10 +0530

    Add files via upload

---
+++ ALGORITHMS/Recursion/Recursion_5_Print_Zig-Zag.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void zigzag(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    cout << n;
+    zigzag(n - 1);
+    cout << n;
+    zigzag(n - 1);
+    cout << n;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    zigzag(n);
+    return 0;
+}
