f47aa20518a23caf86699518a2e1d93726ffb1c0
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2022-01-27 00:40:19 +0530

    6

--- Stack/Stack_6_NEXT_GREATEST_ELEMENT_ON_THE_RIGHT_Approach-2.cpp
+++ Stack/Stack_6_NEXT_GREATEST_ELEMENT_ON_THE_RIGHT_Approach-2.cpp
@@ -1,44 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> NGER(vector<int> arro)
+void display(vector<int> a)
 {
-    vector<int> nge(arro.size());
-    stack<int> st;
-    st.push(0);
-
-    for (int i = 1; i < arro.size(); i++)
-    {
-        while (st.size() > 0 && arro[i] > arro[st.top()])
-        {
-            int pos = st.top();
-            nge[pos] = arro[i];
-            st.pop();
-        }
-        st.push(i);
-    }
-    while (st.size() > 0)
-    {
-        int pos = st.top();
-        nge[pos] = -1;
-        st.pop();
-    }
-
-    return nge;
+	for (int i = 0; i < a.size(); i++)
+	{
+		cout << a[i] << endl;
+	}
+}
+vector<int> solve(vector<int> arr)
+{
+	stack<int> st;
+	vector<int> nge(arr.size());
+	st.push(0);
+	for (int i = 1; i < arr.size(); i++)
+	{
+		while (st.size() > 0 && arr[i] > arr[st.top()])
+		{
+			int pos = st.top();
+			nge[pos] = arr[i];
+			st.pop();
+		}
+		st.push(i);
+	}
+	while (st.size() > 0)
+	{
+		int pos = st.top();
+		nge[pos] = -1;
+		st.pop();
+	}
+	return nge;
 }
 int main()
 {
-    int n;
-    cin >> n;
-    vector<int> iarr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> iarr[i];
-    }
-    vector<int> opt = NGER(iarr);
-    for (int i = 0; i < opt.size(); i++)
-    {
-        cout << opt[i]<<" ";
-    }
-
-    return 0;
+	int n;
+	cin >> n;
+	vector<int> arr(n, 0);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	vector<int> nge(n, 0);
+	nge = solve(arr);
+	display(nge);
+	return 0;
 }
