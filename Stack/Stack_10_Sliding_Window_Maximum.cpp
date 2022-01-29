eb398f42da624bc41e58199def3d7694e8ecc798
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2022-01-29 17:22:30 +0530

    10

---
+++ Stack/Stack_10_Sliding_Window_Maximum.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void solve(vector<long long> arr, long long k)
+{
+
+    stack<long long> st;
+    st.push(arr.size() - 1);
+    vector<long long> nge(arr.size());
+    nge[arr.size() - 1] = arr.size();
+    for (long long i = arr.size() - 2; i >= 0; i--)
+    {
+        while (st.size() > 0 && arr[i] >= arr[st.top()])
+        {
+
+            st.pop();
+        }
+        if (st.size() == 0)
+        {
+
+            nge[i] = arr.size();
+        }
+        else
+        {
+
+            nge[i] = st.top();
+        }
+        st.push(i);
+    }
+
+    long long j = 0;
+    for (long long i = 0; i <= arr.size() - k; i++)
+    {
+        if (j < i)
+        {
+            j = i;
+        }
+        while (nge[j] < i + k)
+        {
+            j = nge[i];
+        }
+        cout << arr[j] << " ";
+    }
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    vector<long long> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    long long k;
+    cin >> k;
+
+    solve(arr, k);
+
+    return 0;
+}
