66d0053d6cb73a4d8cafd983624eaa3cfde3b56e
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2022-01-31 16:32:52 +0530

    11

---
+++ Stack/Stack_11_INFIX_EVALUATION.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int operation(int v1, int v2, char optr)
+{
+    if (optr == '+')
+    {
+        return v1 + v2;
+    }
+    else if (optr == '-')
+    {
+        return v1 - v2;
+    }
+    else if (optr == '*')
+    {
+        return v1 * v2;
+    }
+    else
+    {
+        return v1 / v2;
+    }
+}
+int precedence(char optr)
+{
+    if (optr == '+' || optr == '-')
+    {
+        return 1;
+    }
+    // else if (optr == '-')
+    // {
+    //     return 1;
+    // }
+    // else if (optr == '*')
+    // {
+    //     return 2;
+    // }
+    else
+    {
+        return 2;
+    }
+}
+void solve(string str)
+{
+    stack<int> oprnds;
+    stack<char> opertr;
+    for (int i = 0; i < str.size(); i++)
+    {
+        char ch = str[i];
+        if (ch == '(')
+        {
+            opertr.push(ch);
+        }
+        else if (isdigit(ch))
+        {
+            oprnds.push(ch - '0');
+        }
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+        {
+            while (opertr.size() > 0 && opertr.top() != '(' && precedence(ch) <= precedence(opertr.top()))
+            {
+                char optr = opertr.top();
+                opertr.pop();
+                int v2 = oprnds.top();
+                oprnds.pop();
+                int v1 = oprnds.top();
+                oprnds.pop();
+
+                int val = operation(v1, v2, optr);
+                oprnds.push(val);
+            }
+            opertr.push(ch);
+        }
+        else if (ch == ')')
+        {
+            while (opertr.top() != '(')
+            {
+                char optr = opertr.top();
+                opertr.pop();
+                int v2 = oprnds.top();
+                oprnds.pop();
+                int v1 = oprnds.top();
+                oprnds.pop();
+
+                int val = operation(v1, v2, optr);
+                oprnds.push(val);
+            }
+            opertr.pop();
+        }
+    }
+
+    while (opertr.size() > 0)
+    {
+        char optr = opertr.top();
+        opertr.pop();
+        int v2 = oprnds.top();
+        oprnds.pop();
+        int v1 = oprnds.top();
+        oprnds.pop();
+
+        int val = operation(v1, v2, optr);
+        oprnds.push(val);
+    }
+    cout << oprnds.top();
+    oprnds.pop();
+}
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    solve(str);
+    return 0;
+}
