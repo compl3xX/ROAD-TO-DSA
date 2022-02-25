9cfb865ff84825e75be5a3db8215fdfdd6b8028f
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2022-02-25 23:01:37 +0530

    02

---
+++ TREE/Tree_02_Level_Order_Traversal.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+node *buildTree(node *root)
+{
+    cout << "Enter the data:-";
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+    cout << "Enter data for insertion in left " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for insertion in right" << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+int main()
+{
+    node *root = NULL;
+    root = buildTree(root);
+    cout << "Print Level order traversal" << endl;
+    levelOrderTraversal(root);
+    return 0;
+}
